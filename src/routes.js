5146e5542b01f3ff68a1adfbf9eccad1e1f708e3
github username <github email address>
2016-04-19 15:42:29 -0400

    Initial Commit

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+// mapping for this url show this componenent
+
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+
+import App from './components/app.js'
+import PostsIndex from './components/posts_index'
+import PostsNew from './components/posts_new';
+
+
+export default (
+<Route path="/" component={App}>
+  <IndexRoute component={PostsIndex} />
+  <Route path = "posts/new" component={PostsNew} />
+</Route>
+);
