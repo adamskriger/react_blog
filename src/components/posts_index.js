0142cc36f3a0da215c8a1bcc55c83e9ce352ca9f
github username <github email address>
2016-04-21 19:28:46 -0400

    installed webpack globally

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -9,6 +9,18 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <Link to={"posts/" + post.id}>
+        <span className="pull-xs-right">{post.categories}</span>
+        <strong>{post.title}</strong>
+          </Link>
+        </li>
+      );
+    })
+  }
 
   render() {
     return (
@@ -19,14 +31,22 @@ class PostsIndex extends Component {
           </Link>
 
         </div>
-        List of blog posts
-      </div>
+          <h3>Posts</h3>
+            <ul className="list-group">
+            {this.renderPosts()}
+
+            </ul>
+        </div>
     );
   }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
 }
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators( { fetchPosts } , dispatch);
 }
 
-export default connect(null, mapDispatchToProps)(PostsIndex);
+export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex);
